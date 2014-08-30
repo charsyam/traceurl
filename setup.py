629b9c402c738f0341f7d34bfc11363686e61ec9
clark.kang <clark.kang@kakao.com>
2014-08-30 21:44:25 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='traceurl',
+        version='0.1',
+        description='Python Url Tracing Library',
+        author='DaeMyung Kang',
+        author_email='charsyam@gmail.com',
+        url='https://github.com/charsyam/traceurl',
+        platforms='any',
+        install_requires=['httplib2'],
+        packages=['traceurl'],
+        scripts=['traceurl/__init__.py']
+        )
